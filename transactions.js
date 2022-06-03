9fcf41e0ffd70cc6d3c9434ad26151d9267114c7
Saviee <saviourise1@gmail.com>
2022-06-03 01:51:54 +0100

    newest commit

--- transactions.js
+++ transactions.js
@@ -8,59 +8,63 @@ const addinterest = function addInterest() {
       console.log(err);
     }
 
-      investment.forEach((invest) => {
-
-        if (invest.amount <= 4999 && invest.interest.length < 3){
-          let interests;
-          let period = invest.interest.length + 1
-          let nextdays = period * 10
-          if(moment().format('MM-DD-YYYY') >=moment(invest.approvedAt).add(nextdays, 'days').format('MM-DD-YYYY')){
-              interests = invest.amount * 0.1
-              invest.interest.push(interests)
-              User.findById(invest.user.id, async (err, user)=> {
-                user.profit += interests
-                user.balance += interests
-                user.save()
-              })
-              invest.save()
-
-          }
+    investment.forEach(async (invest) => {
+      if (invest.amount <= 4999 && invest.interest.length < 3) {
+        let interests;
+        let period = invest.interest.length + 1;
+        if (
+          moment().format("MM-DD-YYYY") ==
+          moment(invest.approvedAt).add(period, "months").format("MM-DD-YYYY")
+        ) {
+          interests = invest.amount * 0.1;
+          invest.interest.push(interests);
+          User.findById(invest.user.id, async (err, user) => {
+            user.profit += interests;
+            user.balance += interests;
+            await user.save();
+          });
+          await invest.save();
         }
-
-        else if (invest.amount >=5000 && invest.amount <= 49999 && invest.interest.length < 6){
-          let interests;
-          let period = invest.interest.length + 1
-         let nextdays = period * 10
-          if(moment().format('MM-DD-YYYY') >=moment(invest.approvedAt).add(nextdays, 'days').format('MM-DD-YYYY')){
-              interests = invest.amount * 0.15
-              invest.interest.push(interests)
-              //change
-              User.findById(invest.user.id, async (err, user)=> {
-                user.profit += interests
-                user.balance += interests
-                user.save()
-              })
-              invest.save()
-          }
+      } else if (
+        invest.amount >= 5000 &&
+        invest.amount <= 49999 &&
+        invest.interest.length < 6
+      ) {
+        let interests;
+        let period = invest.interest.length + 1;
+        if (
+          moment().format("MM-DD-YYYY") ==
+          moment(invest.approvedAt).add(period, "months").format("MM-DD-YYYY")
+        ) {
+          interests = invest.amount * 0.15;
+          invest.interest.push(interests);
+          //change
+          User.findById(invest.user.id, async (err, user) => {
+            user.profit += interests;
+            user.balance += interests;
+            await user.save();
+          });
+          await invest.save();
         }
-
-        else if (invest.amount >=50000 && invest.interest.length < 9){
-          let interests;
-          let period = invest.interest.length + 1
-     let nextdays = period * 10
-          if(moment().format('MM-DD-YYYY') >=moment(invest.approvedAt).add(nextdays, 'days').format('MM-DD-YYYY')){
-              interests = invest.amount * 0.2
-              invest.interest.push(interests)
-              //change
-              User.findById(invest.user.id, async (err, user)=> {
-                user.profit += interests
-                user.balance += interests
-                user.save()
-              })
-              invest.save()
-          }
+      } else if (invest.amount >= 50000 && invest.interest.length < 9) {
+        let interests;
+        let period = invest.interest.length + 1;
+        if (
+          moment().format("MM-DD-YYYY") ==
+          moment(invest.approvedAt).add(period, "months").format("MM-DD-YYYY")
+        ) {
+          interests = invest.amount * 0.2;
+          invest.interest.push(interests);
+          //change
+          User.findById(invest.user.id, async (err, user) => {
+            user.profit += interests;
+            user.balance += interests;
+            await user.save();
+          });
+          await invest.save();
         }
-      })
+      }
+    });
     //console.log(investment);
   });
 };
